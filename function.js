7d168e02d8f188fc054844868cbc6155bbef52ef
DeadChannelDice <michael.charpin@gmail.com>
2023-06-06 19:27:43 -0400

    Removing unused beforeAfter functions.

--- function.js
+++ function.js
@@ -1,39 +1,7 @@
 // Code for Before/After Sliders By Webdevtrick ( https://webdevtrick.com )
 function beforeAfter() {
   document.getElementById('alteredone').style.width = document.getElementById('sliderone').value + "%";
-}
 
-function beforeAfterTwo() {
-    document.getElementById('alteredtwo').style.width = document.getElementById('slidertwo').value + "%";
-  }
-
-  function beforeAfterThree() {
-    document.getElementById('alteredthree').style.width = document.getElementById('sliderthree').value + "%";
-  }
-
-  function beforeAfterFour() {
-    document.getElementById('alteredfour').style.width = document.getElementById('sliderfour').value + "%";
-  }
-
-  function beforeAfterFive() {
-    document.getElementById('alteredfive').style.width = document.getElementById('sliderfive').value + "%";
-  }
-
-  function beforeAfterSix() {
-    document.getElementById('alteredsix').style.width = document.getElementById('slidersix').value + "%";
-  }
-
-  function beforeAfterSeven() {
-    document.getElementById('alteredseven').style.width = document.getElementById('sliderseven').value + "%";
-  }
-
-  function beforeAfterHero() {
-    document.getElementById('altered-hero-cover').style.width = document.getElementById('sliderhero').value + "%";
-  }
-
-  function beforeAfter8() {
-    document.getElementById('altered-8').style.width = document.getElementById('slider-8').value + "%";
-  }
 
   const scrollLinks = document.querySelectorAll(".scroll-link");
   scrollLinks.forEach((link) => {
